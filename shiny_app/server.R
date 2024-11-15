7c1b15d2cb300e08e45bd321f782c6a8f821647c
dylanc9918 <93285562+dylanc9918@users.noreply.github.com>
2024-11-15 12:03:14 -0700

    Update and rename server.R to shiny_app/server.R

--- shiny_app/server.R
+++ shiny_app/server.R
@@ -1,5 +1,4 @@
 my_server <- function(input, output, session) {
-  # Call the server function portion of the `cars_table_module.R` module file
   callModule(
     tree_table_module,
     "tree_table"
